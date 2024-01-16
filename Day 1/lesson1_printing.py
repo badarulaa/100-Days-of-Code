a43544cf2c1245b0f35c62a7216d1c8c6c7c9806
Badar <badarul93@gmail.com>
2024-01-16 07:10:56 +0700

    first commit

---
+++ Day 1/lesson1_printing.py
@@ -0,0 +1,14 @@
+# Write a program in main.py that prints the following notes on Python programming into the Output console. Your code should print all three lines.
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')\n")
+
+# print with concatenate type
+print('Hello' + ' ' + 'Badar')
+print('Hello ' + 'Badar')
+print('Hello' + ' Badar')
---
+++ Day 1/lesson3_input_function.py
@@ -0,0 +1,17 @@
+# How Inputs Work in Auditorium
+
+# name1 = input()
+# name2 = input()
+# name3 = input()
+
+print('example of multiplication input')
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
+
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+print('example for count length of name')
+name = input()
+print(len(name))
